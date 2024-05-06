2e4cf728640a77d51676db2ae7656177bce13a7d
Amitha <Amitha@LAPTOP-NPQ5HL7M>
2024-05-06 07:47:37 +0530

    Test Class

---
+++ src/com/obsqura/training/samplepgms/TestClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.obsqura.training.samplepgms;
+
+/**
+ * A pgm to print Hello World
+ * 
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
